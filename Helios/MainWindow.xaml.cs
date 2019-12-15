648bb9e6bae7a06532876ac2950da3e980a57b2d
Friedrich von Never <friedrich@fornever.me>
2019-12-15 22:32:41 +0300

    Add a WPF app template (#4)

---
+++ Helios/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Helios
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
